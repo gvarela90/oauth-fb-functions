31108d16d5881048a7364781e87163e2b2394ddb
Gabriel Aguilar <gabriel.aguilar@nomissolutions.com>
2018-11-08 11:59:36 -0600

    Remove config.ts

--- functions/src/config.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  GOOGLE_AUTHENTICATOR_NAME: "eFin app",
-  RECAPTCHA_SECRET_KEY: "6Ld35nYUAAAAALPXXScdDd-st_EWsF5gKtV3o4KN",
-  FIREBASE_API_KEY: "AIzaSyBtMglwvSgchQG1S5TbzYXmf1KM92KY62I",
-  FIREBASE_AUTH_DOMAIN: "oauth-server-test.firebaseapp.com",
-  FIREBASE_DATABASE_URL: "https://oauth-server-test.firebaseio.com",
-  FIREBASE_PROJECT_ID: "oauth-server-test"
-};
